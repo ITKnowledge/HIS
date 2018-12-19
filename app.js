48673a63dd42065d25e0e271f57f4c00b902c00c
kbellioum <kbellioum@gmail.com>
2018-12-19 20:44:45 +0100

    Switch to mlab for mongodb

--- app.js
+++ app.js
@@ -1,74 +1,75 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session')
-var flash = require('connect-flash');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var flash = require("connect-flash");
 
-var routes = require('./routes/index');
-var users = require('./routes/user');
+var routes = require("./routes/index");
+var users = require("./routes/user");
 
+var mongoose = require("mongoose");
 
-var mongoose = require('mongoose');
+mongoose.connect("mongodb://sih:Azerty123@ds139614.mlab.com:39614/sih");
 
-mongoose.connect('mongodb://localhost/sih');
-
-var app = exports.app = express();
+var app = (exports.app = express());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'supersecret', saveUninitialized: true, resave: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "supersecret", saveUninitialized: true, resave: true })
+);
 app.use(flash());
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/", routes);
+app.use("/users", users);
 
-
-require('./routes/main.js');
+require("./routes/main.js");
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
+      title: "error"
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {},
+    title: "error"
+  });
 });
-
 
 module.exports = app;
